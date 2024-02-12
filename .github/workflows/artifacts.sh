f60e2069972d937a400e19dd6a9f8d2ff8b7ed0f
G. Allegri <giohappy@gmail.com>
2024-02-12 17:55:51 +0100

    initial test workflow

--- .github/workflows/artifacts.sh
+++ .github/workflows/artifacts.sh
@@ -1,47 +1,30 @@
 #!/bin/bash
 
-REGION="eu-south-1"
-BUCKET="artifacts.geonode.org"
-PATH="geoserver/$GEOSERVER_VERSION"
-GIT_BRANCH=$GEOSERVER_VERSION
-PROFILE="jenkins"
+la geoserver-geonode-ext
 
-# Download DATA DIR and custom Libs
-
-pushd src/web/app/target/
-/usr/bin/wget https://github.com/GeoNode/geoserver-geonode-ext/archive/refs/heads/$GIT_BRANCH.zip -O geoserver-geonode-ext-$GIT_BRANCH.zip
-/usr/bin/unzip geoserver-geonode-ext-$GIT_BRANCH.zip
+cp -r geoserver-geonode-ext src/web/app/target/
+pushd geoserver/src/web/app/target/
 
 # Prepare the geoserver.war and geoserver_vanilla.war packages
-/bin/mv geoserver.war geoserver_vanilla.war
-/bin/mkdir _tmp
-pushd _tmp
-/usr/bin/unzip ../geoserver_vanilla.war
+mv geoserver.war geoserver_vanilla.war
+mkdir _tmp
 
-# Replace Libs-* Plugins-* JARs
-## getting rid of clashing/outdated dependencies
-#/bin/rm -Rf WEB-INF/lib/print-lib-*
-#/bin/rm -Rf WEB-INF/lib/asm-3.1.jar
-## copying the new ones
-#/bin/cp ../geoserver-geonode-ext-$GIT_BRANCH/libs/*.jar WEB-INF/lib/
-#/bin/cp ../geoserver-geonode-ext-$GIT_BRANCH/plugins/*.jar WEB-INF/lib/
-#/bin/rm -Rf ../geoserver_vanilla.war
-#/usr/bin/zip -r ../geoserver_vanilla.war ./
+pushd _tmp # src/web/app/target/_tmp
+unzip ../geoserver_vanilla.war
 
-# Replace GEOSERVER_DATA_DIR
-/bin/rm -Rf data/
-/bin/cp -r ../geoserver-geonode-ext-$GIT_BRANCH/data ./
-/usr/bin/zip -r ../geoserver.war ./
+
+cp -r ../geoserver-geonode-ext/data ./
+zip -r ../geoserver.war ./
 popd
 
-# Prepare the geonode-geoserver-ext-web-app-$GEOSERVER_VERSION-data.zip artifact containing the GEOSERVER_DATA_DIR
-pushd geoserver-geonode-ext-$GIT_BRANCH/
-/usr/bin/zip -r ../geonode-geoserver-ext-web-app-data.zip data/
+pushd geoserver-geonode-ext
+
+zip -r ../geonode-geoserver-ext-web-app-data.zip data/
 popd
 
 # Cleaning up
-/bin/rm -Rf _tmp/
-/bin/rm -Rf geoserver-geonode-ext-$GIT_BRANCH.zip
+rm -Rf _tmp/
+
 popd
 
 echo "AWS client id: $AWS_ACCESS_KEY_ID"
