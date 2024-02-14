d82ec6357a0796a3540e26b3c8e7944d31f5c0b4
G. Allegri <giohappy@gmail.com>
2024-02-14 18:51:06 +0100

    include geoserver_vanilla.war artifact

--- .github/workflows/artifacts.sh
+++ .github/workflows/artifacts.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-la geoserver-geonode-ext
-
 cp -r geoserver-geonode-ext geoserver/src/web/app/target/
 pushd geoserver/src/web/app/target/
 
+# Output folder with artifacts
 mkdir artifacts
-# Prepare the geoserver.war and geoserver_vanilla.war packages
-mv geoserver.war geoserver_vanilla.war
-mkdir _tmp
 
+mv geoserver.war geoserver_vanilla.war
+# Copy geoserver_vanilla.wat artifact
+cp geoserver_vanilla.war artifacts/
+
+# Inflate data folder in geonode.war
+mkdir _tmp
 pushd _tmp
 unzip ../geoserver_vanilla.war
-
-
 cp -r ../geoserver-geonode-ext/data ./
 zip -r ../artifacts/geoserver.war ./
 popd
 
+# Create zip artifact for data
 pushd geoserver-geonode-ext
-
 zip -r ../artifacts/geonode-geoserver-ext-web-app-data.zip data/
 popd
 
