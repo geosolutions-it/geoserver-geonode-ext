baef23321b78767a5f54b2186feb22bc8848e2c6
G. Allegri <giohappy@gmail.com>
2024-03-22 17:54:55 +0100

    updated artifact.sh

--- .github/workflows/artifacts.sh
+++ .github/workflows/artifacts.sh
@@ -3,25 +3,36 @@
 cp -r geoserver-geonode-ext geoserver/src/web/app/target/
 pushd geoserver/src/web/app/target/
 
-# Output folder with artifacts
+# output folder with artifacts
 mkdir artifacts
 
 mv geoserver.war geoserver_vanilla.war
-# Copy geoserver_vanilla.wat artifact
+# copy geoserver_vanilla.wat artifact
 cp geoserver_vanilla.war artifacts/
 
-# Inflate data folder in geonode.war
+# inflate data folder in geonode.war
 mkdir _tmp
 pushd _tmp
 unzip ../geoserver_vanilla.war
+
+## get rid of clashing/outdated dependencies
+rm -Rfv WEB-INF/lib/gs-sec-oauth2-core*
+## copy the new ones
+cp -v ../geoserver-geonode-ext/libs/* WEB-INF/lib/
+
+## replace H2 hibernate with PostGIS for the Geofence rules DB
+wget https://repo1.maven.org/maven2/org/postgis/postgis-jdbc/1.3.3/postgis-jdbc-1.3.3.jar -O WEB-INF/lib/postgis-jdbc-1.3.3.jar
+wget https://maven.geo-solutions.it/org/hibernatespatial/hibernate-spatial-postgis/1.1.3.2/hibernate-spatial-postgis-1.1.3.2.jar -O WEB-INF/lib/hibernate-spatial-postgis-1.1.3.2.jar
+rm WEB-INF/lib/hibernate-spatial-h2-geodb-1.1.3.2.jar 
+
 cp -r ../geoserver-geonode-ext/data ./
 zip -r ../artifacts/geoserver.war ./
 popd
 
-# Create zip artifact for data
+# create zip artifact for data
 pushd geoserver-geonode-ext
 zip -r ../artifacts/geonode-geoserver-ext-web-app-data.zip data/
 popd
 
-# Cleaning up
+# cleaning up
 rm -Rf _tmp/
